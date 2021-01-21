a5d2c72f4fdc87e3ac6602976fe6ae77b6860561
sn49 <34567701+sn49@users.noreply.github.com>
2021-01-21 21:11:30 +0900

    first commit

---
+++ braintraing/Assets/Scripts/Stage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage : MonoBehaviour
+{
+    public int cards;
+    public float answertime;
+    public float showtime;
+
+    List<int> numbers = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cards = 3;
+        showtime = 4000;
+        answertime = 4000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ClickStart()
+    {
+        for (int i=0; i<cards; i++)
+        {
+            numbers.Add(Random.Range(1,21));
+        }
+    }
+}
